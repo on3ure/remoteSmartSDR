4963cd5c2502af20e5e4414ee1d03069221a0cd3
Wietse De Ridder <wietse@wieni.be>
2020-10-23 23:03:06 +0200

    Resolve tsx warnings

--- web/src/components/form/Form.tsx
+++ web/src/components/form/Form.tsx
@@ -7,13 +7,6 @@ import Slider from 'components/slider/Slider';
 import SmartSDRFormService from './services/SmartSDRFormService';
 import InputField from './fields/InputField';
 
-interface FormValues {
-  smartSDRip: string,
-  smartSDRport: string,
-  pttDelay: number,
-  offset: number,
-}
-
 const Form = () => {
   const initialValues: FormValues = {
     smartSDRip: '',
@@ -55,7 +48,7 @@ const Form = () => {
                 name="smartSDRip"
                 label="IP address"
                 placeholder="xxx.xxx.x.x"
-                maxLength="15"
+                maxLength={15}
               />
             </Card>
             <Card title="TCP port">
@@ -64,7 +57,7 @@ const Form = () => {
                 name="smartSDRport"
                 label="TCP port"
                 placeholder="xxxx"
-                maxLength="5"
+                maxLength={5}
               />
             </Card>
             <Card title="PTT release delay">
@@ -72,8 +65,8 @@ const Form = () => {
                 id="pttDelay"
                 name="pttDelay"
                 label="Push-to-Talk release delay"
-                min="0"
-                max="4"
+                min={0}
+                max={4}
                 steps={[100, 200, 300, 400 ,500]}
               />
             </Card>
@@ -82,13 +75,13 @@ const Form = () => {
                 id="offset"
                 name="offset"
                 label="Push-to-Talk offset"
-                min="0"
-                max="12"
+                min={0}
+                max={12}
                 steps={[1, 5, 10, 15, 20, 25, 50, 100, 150, 200, 250, 500, 1000]}
               />
             </Card>
           </div>
-          <button type="submit" onClick={handleSubmit} disabled={isSubmitting}>
+          <button type="submit" onClick={() => handleSubmit()} disabled={isSubmitting}>
             Submit
           </button>
         </div>
@@ -96,5 +89,12 @@ const Form = () => {
     </Formik>
   );
 };
+
+interface FormValues {
+  smartSDRip: string,
+  smartSDRport: string,
+  pttDelay: number,
+  offset: number,
+}
 
 export default Form;
