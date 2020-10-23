0b76d7d6a0bdd09833db9f4107ec1a30f3ad9d94
Steven De Boeck <steven.deboeck@crodeon.com>
2020-10-23 22:24:13 +0200

    Add hello api endpoint to save form data

--- web/pages/api/hello.js
+++ web/pages/api/hello.js
@@ -1,6 +1,46 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import Redis from 'ioredis';
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+const redis = new Redis(process.env.REDIS_PORT, process.env.REDIS_HOST); 
+
+export default async (req, res) => {
+  const {
+    body,
+    method,
+  } = req
+
+  const data = {
+    ip: null,
+    port: null,
+    delay: null,
+    offset: null,
+  };
+  
+  switch (method) {
+    case 'GET':
+      const keys =  Object.keys(data);
+      const pipe = keys.map(key => ['get', key]);
+
+      await redis
+      .pipeline(pipe)
+      .exec()
+      .then((result) => {
+        result.forEach((item, key) => {
+          if (item[0]) {
+            console.error(item[0]);
+            return;
+          }
+
+          data[keys[key]] = item[1];
+        });
+      });
+      res.status(200).json(data)
+      break
+    case 'PUT':
+      await redis.mset(body);
+      res.status(200).json(body)
+      break
+    default:
+      res.setHeader('Allow', ['GET', 'PUT'])
+      res.status(405).end(`Method ${method} Not Allowed`)
+  }
 }
