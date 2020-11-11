98f60d5504c74dc27a4c90f2a0e75e992a44a885
Mathias De Ridder <mathias.deridder@luon.com>
2020-11-11 14:44:20 +0100

    Added navigation component

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 
 import { Main } from 'pages/Main';
+import { Nav } from 'components/nav/Nav';
 
 const Home = () => {
   return (
@@ -10,6 +11,8 @@ const Home = () => {
         <link rel="icon" href="/favicon.png" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" />
       </Head>
+
+      <Nav />
 
       <main>
         <div className="container">
