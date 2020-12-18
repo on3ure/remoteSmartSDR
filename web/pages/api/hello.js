e61d2cc89a5ac358e1e8075bdd948b65bd79a4e2
Mathias De Ridder <mathias.deridder@luon.com>
2020-12-18 21:40:49 +0100

    Added pttDelay to api

--- web/pages/api/hello.js
+++ web/pages/api/hello.js
@@ -11,6 +11,7 @@ export default async (req, res) => {
   const data = {
     smartSDRip: '',
     smartSDRport: '',
+    pttDelay: '',
     cloudlogEnabled: false,
     cloudlogURL: '',
     cloudlogAPIkey: '',
